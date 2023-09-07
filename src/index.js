c54dffe299be2a184f091d3f4d52877a50632b6f
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-09-07 11:12:59 +0900

    Add JSON file comments for initial planning

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './style.css';
 import '@fontsource/rubik';
 import '@fontsource/rubik/400.css';
 
+// function to create table elements
+// function to create import data from json
+// loop through imported data and populate DOM using create table elements
